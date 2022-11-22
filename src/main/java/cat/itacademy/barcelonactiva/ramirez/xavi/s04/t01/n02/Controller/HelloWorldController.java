57d989e17ce1c7e329a6f8a1671f6cac32ff2758
XaviRamireZ077 <ramirezinfantesxavi@gmail.com>
2022-11-22 19:10:56 +0100

    Initial commit - entrega proyecto gradle

---
+++ src/main/java/cat/itacademy/barcelonactiva/ramirez/xavi/s04/t01/n02/Controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package cat.itacademy.barcelonactiva.ramirez.xavi.s04.t01.n02.Controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/HelloWorld")
+    public String Saluda(@RequestParam(defaultValue = "UNKNOWN") String nom) {
+
+        return "Hola, " + nom + "Estas executant un projecte Gradle";
+    }
+
+    @RequestMapping(value = {"/HelloWorld2","/HelloWorld2/{nom}"})
+    public String Saluda2(@PathVariable(required = false) String nom) {
+        return "Hola, " + nom + "Estas executant un projecte Gradle";
+
+    }
+}
